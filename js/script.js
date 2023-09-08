8636baf4bdfa1790aa46429b297a663450159115
zachjacob2002 <143147128+zachjacob2002@users.noreply.github.com>
2023-09-08 10:34:33 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,15 @@ document.addEventListener("DOMContentLoaded", function () {
     let diffTotal = firstNum - secondNum; // Corrected the variable name
     document.getElementById("diffanswer").innerHTML = `${firstNum} - ${secondNum}, equals to ${diffTotal}`;
   }
+  
+  function displayMult() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML);
+  let secondNum = Number(document.getElementById('secondNum').innerHTML);
+  let product = firstNum * secondNum;
+  document.getElementById("productanswer").innerHTML = `${firstNum} * ${secondNum}, equals to ${product}`;
+  }
 
+  document.getElementById('MultButton').addEventListener("click", displayMult);
   document.getElementById('SumButton').addEventListener("click", displaySum);
   document.getElementById('DiffButton').addEventListener("click", displayDiff);
 });
