3c4ac3e9008410afb828765984f4beb89685a428
zachjacob2002 <143147128+zachjacob2002@users.noreply.github.com>
2023-09-08 10:10:36 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,4 +6,8 @@ function displaySum() {
   document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
 }
 
-document.getElementById('sumButton').addEventListener("click", displaySum);
+function displayDiff() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML)
+  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+  let total = firstNum - secondNum;
+document.getElementById('sumButton').addEventListener("click", displayDIff);
