60571e00330e256ee1bbf9bf6ba0e5ed26fedd1b
zachjacob2002 <143147128+zachjacob2002@users.noreply.github.com>
2023-09-08 10:14:29 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 function displaySum() {
-  let firstNum = Number(document.getElementById('firstNum').innerHTML)
-  let secondNum = Number(document.getElementById('secondNum').innerHTML)
-
+  let firstNum = Number(document.getElementById('firstNum').innerHTML);
+  let secondNum = Number(document.getElementById('secondNum').innerHTML);
   let total = firstNum + secondNum;
-  document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
+  document.getElementById("answer").innerHTML = `${firstNum} + ${secondNum}, equals to ${total}`;
 }
 
 function displayDiff() {
-  let firstNum = Number(document.getElementById('firstNum').innerHTML)
-  let secondNum = Number(document.getElementById('secondNum').innerHTML)
-  let total = firstNum - secondNum;
-document.getElementById('sumButton').addEventListener("click", displayDIff);
+  let firstNum = Number(document.getElementById('firstNum').innerHTML);
+  let secondNum = Number(document.getElementById('secondNum').innerHTML);
+  let diffTotal = firstNum - secondNum; // Corrected the variable name
+  document.getElementById("answer").innerHTML = `${firstNum} - ${secondNum}, equals to ${diffTotal}`;
+}
+
+document.getElementById('SumButton').addEventListener("click", displaySum); // Corrected the button ID
+document.getElementById('DiffButton').addEventListener("click", displayDiff); // Corrected the button ID
