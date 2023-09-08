97683c616e3fa7015ed4b8a8d9da9669c551691d
zachjacob2002 <143147128+zachjacob2002@users.noreply.github.com>
2023-09-08 10:29:01 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
-function displaySum() {
-  let firstNum = Number(document.getElementById('firstNum').innerHTML);
-  let secondNum = Number(document.getElementById('secondNum').innerHTML);
-  let total = firstNum + secondNum;
-  document.getElementById("sumanswer").innerHTML = `${firstNum} + ${secondNum}, equals to ${total}`;
-}
+document.addEventListener("DOMContentLoaded", function () {
+  // Your code here, including the displaySum and displayDiff functions
+  function displaySum() {
+    let firstNum = Number(document.getElementById('firstNum').innerHTML);
+    let secondNum = Number(document.getElementById('secondNum').innerHTML);
+    let total = firstNum + secondNum;
+    document.getElementById("sumanswer").innerHTML = `${firstNum} + ${secondNum}, equals to ${total}`;
+  }
 
-function displayDiff() {
-  let firstNum = Number(document.getElementById('firstNum').innerHTML);
-  let secondNum = Number(document.getElementById('secondNum').innerHTML);
-  let diffTotal = firstNum - secondNum; // Corrected the variable name
-  document.getElementById("diffanswer").innerHTML = `${firstNum} - ${secondNum}, equals to ${diffTotal}`;
-}
+  function displayDiff() {
+    let firstNum = Number(document.getElementById('firstNum').innerHTML);
+    let secondNum = Number(document.getElementById('secondNum').innerHTML);
+    let diffTotal = firstNum - secondNum; // Corrected the variable name
+    document.getElementById("diffanswer").innerHTML = `${firstNum} - ${secondNum}, equals to ${diffTotal}`;
+  }
 
-document.getElementById('SumButton').addEventListener("click", displaySum); // Corrected the button ID
-document.getElementById('DiffButton').addEventListener("click", displayDiff); // Corrected the button ID
+  document.getElementById('SumButton').addEventListener("click", displaySum);
+  document.getElementById('DiffButton').addEventListener("click", displayDiff);
+});
